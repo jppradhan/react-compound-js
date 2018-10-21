3dbe47acaf87a0b697a6393d8024b858eec4c8a8
Jyoti Prakash <er.jppradhan@gmail.com>
2018-10-21 22:41:02 +0530

    popover added with basic animation

--- src/components/PopOver/PopOver.tsx
+++ src/components/PopOver/PopOver.tsx
@@ -19,10 +19,14 @@ export class PopOver extends React.Component<Props, State> {
     };
   }
 
+  public componentWillUnmount() {
+    document.removeEventListener("click", this.onClickOutside);
+  }
+
   public render() {
     const { children, content, position = "bottom", size = "md" } = this.props;
     return (
-      <div className="popover__container" onClick={this.togglePopup}>
+      <div className="popover__container" onClick={this.openPopup}>
         {children}
         {this.state.isShowing ? (
           <div className={`popover__wrapper ${position} ${size}`}>
@@ -33,9 +37,39 @@ export class PopOver extends React.Component<Props, State> {
     );
   }
 
-  private togglePopup = () => {
-    this.setState({
-      isShowing: !this.state.isShowing
-    });
+  private openPopup = () => {
+    this.setState(
+      {
+        isShowing: true
+      },
+      () => {
+        document.addEventListener("click", this.onClickOutside);
+      }
+    );
+  };
+
+  private closePopup = () => {
+    this.setState(
+      {
+        isShowing: false
+      },
+      () => {
+        document.removeEventListener("click", this.onClickOutside);
+      }
+    );
+  };
+
+  private onClickOutside = (e: Event) => {
+    //@ts-ignore
+    const paths = e.path;
+    for (let i = 0; i < paths.length; i += 1) {
+      if (
+        paths[i].classList &&
+        paths[i].classList.contains("popover__container")
+      ) {
+        return;
+      }
+    }
+    this.closePopup();
   };
 }
