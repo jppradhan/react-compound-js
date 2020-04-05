6e684900955508638118f272fc7a4c7d1f6db55d
Jyoti Prakash <jyotiprakash.pradhan@vimeo.com>
2020-04-05 17:04:55 +0530

    build and version updated

---
+++ dist/components/Crop/styles.d.ts
@@ -0,0 +1,15 @@
+/// <reference types="react" />
+export declare const StyledImage: import("styled-components").StyledComponent<"img", any, {
+    style: import("react").CSSProperties | undefined;
+}, "style">;
+export declare const OverlayImage: import("styled-components").StyledComponent<"img", any, {
+    style: import("react").CSSProperties | undefined;
+}, "style">;
+export declare const Container: import("styled-components").StyledComponent<"div", any, {}, never>;
+export declare const Overlay: import("styled-components").StyledComponent<"div", any, {}, never>;
+export declare const InputFile: import("styled-components").StyledComponent<"input", any, {}, never>;
+export declare const ImageWrappper: import("styled-components").StyledComponent<"div", any, {}, never>;
+export declare const ActionWrapper: import("styled-components").StyledComponent<"div", any, {}, never>;
+export declare const Clipper: import("styled-components").StyledComponent<"div", any, {
+    style: import("react").CSSProperties | undefined;
+}, "style">;
